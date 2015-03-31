5d0bfb44e87cf4db0dc14cafff566257c03ac423
Ali Abou-Zahr <ali.a.zahr@gmail.com>
2015-03-31 16:38:44 -0700

    csv methods added for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ActiveRecord::Base
   has_many :categories, through: :relations
   has_many :relations
 
+  def self.import(file)
+    CSV.foreach("#{Rails.root}/db/csv/mini_proj-items 2.csv", headers: true) do |row|
+      Item.create! row.to_hash
+    end
+  end 
 end
